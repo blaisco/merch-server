fc6e9217fcd1b927b250d342f49ca4ada74e6b48
Scott Blaine <scott@scottblaine.com>
2011-12-17 08:54:46 -0600

    New rake task for creating a product so that I don't keep pinging Valve's server.

--- app/jobs/product_job.rb
+++ app/jobs/product_job.rb
@@ -1,16 +1,66 @@
 class ProductJob
   @queue = :products
+  
+  #~ "{
+    #~ \"name\": \"Portal 2 Collectors Edition Guide\", 
+    #~ \"url\": \"http://store.valvesoftware.com/product.php?i=P0909\", 
+    #~ \"image_urls\": [
+      #~ \"http://www.example.com/main_images/P0909main.png\"
+    #~ ], 
+    #~ \"summary\": \"This strictly limited collector\\u0092s [snip].\", 
+    #~ \"inventory\": [
+      #~ {
+        #~ \"color\": null, 
+        #~ \"sale_price\": \"$24.99\", 
+        #~ \"price\": \"$29.99\", 
+        #~ \"in_stock\": true, 
+        #~ \"size\": null
+      #~ }
+    #~ ],
+    #~ \"images\": [
+      #~ {
+        #~ \"url\": \"http://www.example.com/main_images/P0909main.png\", 
+        #~ \"path\": \"full/e7082274593a43382d1d1424ec4ea9b540569e06.jpg\", 
+        #~ \"checksum\": \"f3b00dc98d85a335be4aa95a41d37520\"
+      #~ }
+    #~ ],
+    #~ \"description\": \"This strictly limited collector\\u0092s [snip]"\"
+  #~ }"
+
 
   def self.perform(object)
     product = Product.find_or_initialize_by_url(object["url"])
-    unless product.status == 'inactive'
-      product.name = object["name"]
-      product.summary = object["summary"]
-      product.description = object["description"]
-      # Mark as updated even if no changes; I want to know when a product is
-      # stale (i.e. no one is trying to update it).
-      product.updated_at = Time.now
+    unless product.status == 'inactive' # active or pending
+      product = create_product(product, object)
+      product = create_inventory(product, object["inventory"])
+      product = create_images(product, object["images"])
       product.save
     end
+  end
+  
+  def self.create_product(product, object)
+    product.name = object["name"]
+    product.summary = object["summary"]
+    product.description = object["description"]
+    # Mark as updated even if no changes; I want to know when a product is
+    # stale (i.e. no one is trying to update it).
+    product.updated_at = Time.now
+    product
+  end
+  
+  def self.create_inventory(product, object)
+    object.each do |inv|
+      
+    end
+    product
+  end
+  
+  def self.create_images(product, object)
+    unless object.nil? # might not have images for a product
+      object.each do |img|
+        
+      end
+    end
+    product
   end
 end
---
+++ lib/tasks/api.rake
@@ -0,0 +1,14 @@
+namespace :api do
+  namespace :product do
+    task :create => :environment do
+      json_str = "{\"description\": \"This strictly limited collector\\u0092s edition of the official Portal 2 Game Guide features additional art, a hardcover binding and two bookmarks.\\r\\nPublisher: Future Press\\r\\nISBN: 9783869930381\\r\\nPages: 384\\r\\nSize: 8 1/4\\\" x 11 1/2\\\"\", \"url\": \"http://store.valvesoftware.com/product.php?i=P0909\", \"image_urls\": [\"http://store.valvesoftware.com/product_images/main_images/P0909main.png\"], \"summary\": \"This strictly limited collector\\u0092s edition of the official Portal 2 Game Guide features additional art, a hardcover binding and two bookmarks.\", \"inventory\": [{\"color\": null, \"price\": \"2999\", \"currency\": \"USD\", \"in_stock\": true, \"sale_price\": \"2499\", \"size\": null}], \"images\": [{\"url\": \"http://store.valvesoftware.com/product_images/main_images/P0909main.png\", \"path\": \"full/e7082274593a43382d1d1424ec4ea9b540569e06.jpg\", \"checksum\": \"f3b00dc98d85a335be4aa95a41d37520\"}], \"name\": \"Portal 2 Collectors Edition Guide\"}"
+      
+      object = JSON.parse(json_str)
+      ProductJob.perform(object)
+
+      puts "====== Product JSON ======"
+      puts "#{object.to_json}"
+      puts "============================"
+    end
+  end
+end
