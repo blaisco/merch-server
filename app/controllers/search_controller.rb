0860931f7a9891193946db23738914162e557571
Scott Blaine <scott@scottblaine.com>
2012-01-21 20:56:51 -0600

    Limiting search results by facet works now; yay

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,17 @@ class SearchController < ApplicationController
   def index
   	@query = params[:q] || ""
     terms = clean_query(@query)
+    facets = {}
+    [:game, :franchise, :developer, :merchant, :product_type].each do |f|
+      p = f.to_s.pluralize.to_sym
+      facets[f] = params[p].split(',') if params[p]
+    end
     @products = Product.search(terms, 
           :populate => true, 
           :page => params[:page], 
           :per_page => 40, 
-          :include => [:merchant, :images, :figures])
-          #:with => {:game => [13,12]})
+          :include => [:merchant, :images, :figures],
+          :with => facets)
     @facets = @products.facets
     #Search.create(:query => terms, :ip_address => request.remote_ip, :num_results => @products.size)
   rescue Riddle::ConnectionError
